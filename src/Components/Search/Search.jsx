39c220a43db36834291b7cf8fe9f9d83b0835504
Martinelli-89 <99336076+Martinelli-89@users.noreply.github.com>
2022-12-29 12:43:15 +0000

    added geolocation - missed files

---
+++ src/Components/Search/Search.jsx
@@ -0,0 +1,17 @@
+import "./Search.scss";
+
+export const Search = () => {
+
+    return (
+
+        <div className="search">
+            <input 
+                className="search__input"
+                placeholder="Search location weather..."
+            ></input>
+        </div>
+
+    );
+
+}
+
