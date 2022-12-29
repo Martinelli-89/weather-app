195b73f56798d2f783e175613d5be7a5c23000f8
Martinelli-89 <99336076+Martinelli-89@users.noreply.github.com>
2022-12-29 12:42:48 +0000

    added geolocation

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,29 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 
-const App = () => {
+export const App = () => {
+
+  const [coordinates, setCoordinates] = 
+    useState ({
+  });
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(function(position) {
+    console.log(position);
+    setCoordinates({
+      lat: position.coords.latitude,
+      long: position.coord.longitude
+    })
+    })
+  },[])
 
   return (
 
     <div className="App">
+        <p>
+        Latitude: {coordinates.lat}
+        Longitude: {coordinates.long}
+        </p>
     </div>
 
   );
